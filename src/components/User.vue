c78c9e9584d990e07c81daa896ebde72fbdf03ed
Renan Andrade <eu@renanandrade.com.br>
2017-12-19 23:51:52 -0200

    add validation to props

--- src/components/User.vue
+++ src/components/User.vue
@@ -2,10 +2,11 @@
     <div class="component">
         <h1>The User Component</h1>
         <p>I'm an awesome User!</p>
+        <button @click="changeName">Change my Name</button>
         <hr>
         <div class="row">
             <div class="col-xs-12 col-sm-6">
-                <app-user-detail></app-user-detail>
+                <app-user-detail :myName="name"></app-user-detail>
             </div>
             <div class="col-xs-12 col-sm-6">
                 <app-user-edit></app-user-edit>
@@ -15,15 +16,25 @@
 </template>
 
 <script>
-    import UserDetail from './UserDetail.vue';
-    import UserEdit from './UserEdit.vue';
+  import UserDetail from './UserDetail.vue';
+  import UserEdit from './UserEdit.vue';
 
-    export default {
-        components: {
-            appUserDetail: UserDetail,
-            appUserEdit: UserEdit
-        }
+  export default {
+    data: () => {
+      return {
+        name: 'Renan'
+      }
+    },
+    methods: {
+      changeName() {
+        this.name = 'Bruna'
+      }
+    },
+    components: {
+      appUserDetail: UserDetail,
+      appUserEdit: UserEdit
     }
+  }
 </script>
 
 <style scoped>
--- src/components/UserDetail.vue
+++ src/components/UserDetail.vue
@@ -2,10 +2,24 @@
     <div class="component">
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
+        <p>User Name: {{ switchName() }}</p>
     </div>
 </template>
 
 <script>
+  export default {
+    props: {
+      myName: {
+        type: String,
+        required: true
+      }
+    },
+    methods: {
+      switchName: function() {
+        return this.myName.split("").reverse().join("");
+      }
+    }
+  }
 </script>
 
 <style scoped>
